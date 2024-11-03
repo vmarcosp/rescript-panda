1b8bb5223ee59ab7367d8a4d19e96ca88e88f02e
Marcos Oliveira <marcos.oliveira@vtex.com>
2024-11-03 16:27:14 -0300

    chore: updated build script

--- scripts/build.sh
+++
@@ -1,3 +0,0 @@
-pnpm -F @rescript-panda/generator run rs:build
-pnpm -F @rescript-panda/bindings run rs:build
-pnpm -F @rescript-panda/cli run build
--- scripts/clean-rescript-packages.sh
+++
@@ -1,3 +0,0 @@
-pnpm -F @rescript-panda/generator run rs:clean
-pnpm -F @rescript-panda/cli run rs:clean
-pnpm -F @rescript-panda/bindings run rs:clean
